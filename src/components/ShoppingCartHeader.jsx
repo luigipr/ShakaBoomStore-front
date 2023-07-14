4d8212563720bff5c968e73012e1ac69f8de405a
Gisele Macieira <giselefchaves@gmail.com>
2023-07-14 16:51:44 -0300

    layout da página de carrinho de compras

--- src/components/ShoppingCartHeader.jsx
+++ src/components/ShoppingCartHeader.jsx
@@ -1,38 +1,35 @@
-import styled from "styled-components"
-import useToken from "../hooks/useToken"
-import home from "../assets/Home.jpg"
+import styled from "styled-components";
+import home from "../assets/home-transparente.png";
 
 
 export default function ShoppingCartHeader() {
-
-    const {user} = useToken();
-
-    console.log(user);
-
+    const user = JSON.parse(localStorage.getItem("user"));
+    
     return(
         <HeaderContainer>
-            <div>Nome</div>
+            <div>Carrinho de {user}</div>
             <img src = {home}/>
         </HeaderContainer>
     )
 }
 
 const HeaderContainer = styled.div`
+    background-color: orange;
     width: 100%;
     height: 60px;
     display: flex;
     align-items: center;
+    text-align: center;
     justify-content: space-between;
     position: fixed;
     top: 0px;
     left:0px;
     z-index: 2;
     div{
-        margin-left: 23px;
+        margin-left: 5%;
     }
     img{
         width: 30px;
-        height: 30px;
-        margin-right: 22px;
+        margin-right: 5%;
     }
 `
--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,29 +1,70 @@
-//import styled from "styled-components";
-
 import styled from "styled-components";
 import ShoppingCartHeader from "../components/ShoppingCartHeader";
 import logo from "../assets/logo-transparente.png";
 import trash from "../assets/trash.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { createConfig } from "../services/api";
 
 export default function ShoppingCartPage(){
+    const [carrinho, setCarrinho] = useState([]);
+    const [deleted, setDeleted] = useState(false);
+    const BASE_URL = import.meta.env.VITE_API_URL
+    const user = JSON.parse(localStorage.getItem("user"));
+    const token = JSON.parse(localStorage.getItem("token"));
+    console.log(token)
+
+    function deletarProduto(productId){
+        const url = `${BASE_URL}/shoppingcart/${productId}`;
+        const headers = createConfig(token);
+        axios.delete(url, headers)
+            .then(resp => {
+                console.log(resp.data);
+                if(deleted){
+                    setDeleted(false)
+                }else{
+                    setDeleted(true);
+                }
+            })
+            .catch(err => {
+                console.log(err.response.data);
+            })
+    }
+    useEffect(() => {
+        const url = `${BASE_URL}/shoppingcart`;
+        const headers = createConfig(token);
+        axios.get(url, headers)
+            .then(resp => {
+                console.log(resp.data);
+                setCarrinho(resp.data);
+            })
+            .catch(err => {
+                console.log(err.response.data)
+            })
+    }, [deleted])
     return(
         <>
         <ShoppingCartHeader />
         <ShoppingCartContainer>
-            
-            <ProductContainer>
-                <img src = {logo} />
-                <InfoContainer>
-                    <p>Nome</p>
-                    <p>Descrição</p>
-                    <p>Preço</p>
-                    <p>Quantidade</p>
-                    <img src = {trash} />
-                </InfoContainer>
-            </ProductContainer>
+        
+            {carrinho.map(produto => (
+                <ProductContainer key = {produto.productId}>
+                    <img src = {produto.productImage} />
+                    <InfoContainer>
+                        <p><strong>Descrição: </strong>{produto.description}</p>
+                        <p><strong>Preço: </strong>{produto.price}</p>
+                        <p><strong>Quantidade: </strong>{produto.quantity}</p>
+                        <img src = {trash} onClick={() => deletarProduto(produto.productId)}/>
+                    </InfoContainer>
+                </ProductContainer>)
+            )} 
 
+            <BuyButton>
+                Comprar
+            </BuyButton>
 
-        </ShoppingCartContainer>
+            </ShoppingCartContainer>
+       
         </>
     );
 }
@@ -41,7 +82,6 @@ const ShoppingCartContainer = styled.div`
 const ProductContainer = styled.div`
     background-color: #c5e8c5;
     width: 90%;
-    height: 130px;
     border-radius: 15px;
     margin-top: 10px;
     margin-bottom: 10px;
@@ -50,7 +90,13 @@ const ProductContainer = styled.div`
     justify-content: space-between;
     position: relative;
     img {
-        width: 30%;
+        height: 120px;
+        margin-left: 10px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    strong{
+        font-weight: 800;
     }
 `
 const InfoContainer = styled.div`
@@ -64,12 +110,24 @@ const InfoContainer = styled.div`
     align-items: flex-start;
     position: relative;
     p{
-        margin: 6px;
+        margin: 4px;
+        margin-top: 15px;
+        padding-right: 16px;
     }
     img{
         position: absolute;
         top: 2px;
         right: 2px;
-        width: 5%;
+        width: 16px;
+        height: 19px;
     }
+`
+
+const BuyButton = styled.button`
+    background-color: green;
+    width: 120px;
+    border-radius: 25px;
+    border: none;
+    position: fixed;
+    bottom: 15px;
 `
