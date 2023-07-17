dd84ac3529d3a2c9c00af1487efbfd9a6cc58dc0
Gisele Macieira <giselefchaves@gmail.com>
2023-07-17 11:32:18 -0300

    ajustes na página principal e ligação entre páhina de produtos e carrinho (adição de produto no carrinho

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,52 +2,102 @@ import styled from "styled-components";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import shoppingcart from "../assets/shoppingcart.png";
+import { createConfig } from "../services/api";
 
 export default function Product() {
     const [products, setProducts] = useState();
-    const url = import.meta.env.VITE_API_URL
+    const BASE_URL = import.meta.env.VITE_API_URL;
+    const token = JSON.parse(localStorage.getItem("token"));
 
     useEffect(() => {
-        const promise = axios.get(`${url}/getProducts`)
-        promise.then((res) => { setProducts(res.data) })
+        const promise = axios.get(`${BASE_URL}/products`)
+        promise.then((res) => { 
+            console.log(res.data);
+            setProducts(res.data) 
+        })
         promise.catch((err) => { console.log(err) })
     }, [])
+
+    function addProduto(productId, productImage, price, description, quantity){
+        //body: {productId, productImage, price, description, quantity}
+        //product: {productImage, price, description, quantity, productId, _id}
+        const headers = createConfig(token);
+        const url = `${BASE_URL}/shoppingcart`;
+        const body = {
+            productId,
+            productImage, 
+            price, 
+            description, 
+            quantity
+        }
+        axios.post(url, body, headers)
+            .then(resp => {
+                console.log(resp.data);
+                alert('produto adicionado no carrinho com sucesso!');
+            })
+            .catch(err => {
+                alert(err.response.data);
+            })
+    }
 
     return (
         <>
             {products && products.map((product, index) => (
-                <ProductHolder key={index}>
-                    <ProductImage src={product.productImage} />
-                    <h1>{product.description}</h1>
-                </ProductHolder>
+                <ProductContainer key={index}>
+                    <img src={product.productImage} />
+                    <InfoContainer>
+                        <p><strong>Descrição: </strong>{product.description}</p>
+                        <p><strong>Preço: </strong>R$ {product.price.toFixed(2).toString().replace('.',',')}</p>
+                        <img src = {shoppingcart} onClick={() => addProduto(product.productId, product.productImage, product.price, product.description, product.quantity)}/>
+                    </InfoContainer>
+                </ProductContainer>
             ))}
         </>
     )
 }
 
-const ProductHolder = styled.div`
-    background-color:white;
-    width: 45vw;
-    height: 200px;
-    margin-left: 8px;
-    margin-right: 8px;
-    margin-top: 20px;
-    margin-bottom: 5px;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    filter: drop-shadow(4px 4px 4px rgba(0,0,0,0.25));
-    border-radius:10px;
-    border: 1px solid green;
-    overflow: clip;
-    h1{
-        color:rgba(0,0,0,0.75);
-        font-size: 20px;
-        margin-top:5px;
+const ProductContainer = styled.div`
+    background-color: white;
+    width: 90%;
+    border-radius: 15px;
+    margin-top: 7px;
+    margin-bottom: 7px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    position: relative;
+    img {
+        height: 120px;
+        margin-left: 10px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    strong{
+        font-weight: 800;
     }
 `
 
-const ProductImage = styled.img`
-    height: 70%;
+const InfoContainer = styled.div`
     width: 100%;
+    height: 92%;
+    display: flex;
+    margin-right: 5px;
+    margin-left: 5px;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    position: relative;
+    p{
+        margin: 4px;
+        margin-top: 15px;
+        margin-right: 30px;
+    }
+    img{
+        position: absolute;
+        bottom: -9px;
+        right: 2px;
+        width: 16px;
+        height: 19px;
+    }
 `
